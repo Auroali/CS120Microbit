53e0955e10c6d5737fa706ebc32214c7df54ec8d
Dawn <dawn.auroali@gmail.com>
2023-04-13 12:38:25 -0300

    Initial Commit

---
+++ src/serial.rs
@@ -0,0 +1,23 @@
+use core::fmt;
+use core::fmt::Write;
+use core::ops::Add;
+use microbit::hal::uarte::{Instance, UarteRx, UarteTx};
+use microbit::hal::Uarte;
+
+pub struct UartePort<T: Instance>(UarteTx<T>, UarteRx<T>);
+
+static mut TX_BUF: [u8; 1] = [0; 1];
+static mut RX_BUF: [u8; 1] = [0; 1];
+
+impl<T: Instance> UartePort<T> {
+    pub fn new(serial: Uarte<T>) -> UartePort<T> {
+        let (tx, rx) = serial
+            .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
+            .unwrap();
+        UartePort(tx, rx)
+    }
+
+    pub fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.0.write_str(s)
+    }
+}
